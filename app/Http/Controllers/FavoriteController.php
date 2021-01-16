7695330a80805a323ab52cacbcb7b5599d788242
breney <brunopy2@gmail.com>
2021-01-16 21:28:11 +0000

    #18 Controllers created

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FavoriteController extends Controller
+{
+    /**
+     * Put a user in the favorites list
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function favorite(Request $request)
+    {
+        // check action [star/unstar]
+        if (Chatify::inFavorite($request['user_id'])) {
+            // UnStar
+            Chatify::makeInFavorite($request['user_id'], 0);
+            $status = 0;
+        } else {
+            // Star
+            Chatify::makeInFavorite($request['user_id'], 1);
+            $status = 1;
+        }
+
+        // send the response
+        return Response::json([
+            'status' => @$status,
+        ], 200);
+    }
+
+    /**
+     * Get favorites list
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getFavorites(Request $request)
+    {
+        $favoritesList = null;
+        $favorites = Favorite::where('user_id', Auth::user()->id);
+        foreach ($favorites->get() as $favorite) {
+            // get user data
+            $user = User::where('id', $favorite->favorite_id)->first();
+            $favoritesList .= view('Chatify::layouts.favorite', [
+                'user' => $user,
+            ]);
+        }
+        // send the response
+        return Response::json([
+            'favorites' => $favorites->count() > 0
+                ? $favoritesList
+                : '<p class="message-hint"><span>Your favorite list is empty</span></p>',
+        ], 200);
+    }
+
+    /**
+     * Search in messenger
+     *
+     * @param Request $request
+     * @return void
+     */
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Authinticate the connection for pusher
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function pusherAuth(Request $request)
+    {
+        // Auth data
+        $authData = json_encode([
+            'user_id' => Auth::user()->id,
+            'user_info' => [
+                'name' => Auth::user()->name
+            ]
+        ]);
+        // check if user authorized
+        if (Auth::check()) {
+            return Chatify::pusherAuth(
+                $request['channel_name'],
+                $request['socket_id'],
+                $authData
+            );
+        }
+        // if not authorized
+        return new Response('Unauthorized', 401);
+    }
+
+    /**
+     * Fetch data by id for (user/group)
+     *
+     * @param Request $request
+     * @return collection
+     */
+    public function idFetchData(Request $request)
+    {
+        // Favorite
+        $favorite = Chatify::inFavorite($request['id']);
+
+        // User data
+        if ($request['type'] == 'user') {
+            $fetch = User::where('id', $request['id'])->first();
+        }
+
+        // send the response
+        return Response::json([
+            'favorite' => $favorite,
+            'fetch' => $fetch,
+            'user_avatar' => asset('/storage/' . config('chatify.user_avatar.folder') . '/' . $fetch->avatar),
+        ]);
+    }
+
+    /**
+     * Get contacts list
+     *
+     * @param Request $request
+     * @return JSON response
+     */
+    public function getContacts(Request $request)
+    {
+        // get all users that received/sent message from/to [Auth user]
+        $users = Message::join('users',  function ($join) {
+            $join->on('messages.from_id', '=', 'users.id')
+                ->orOn('messages.to_id', '=', 'users.id');
+        })
+            ->where('messages.from_id', Auth::user()->id)
+            ->orWhere('messages.to_id', Auth::user()->id)
+            ->orderBy('messages.created_at', 'desc')
+            ->get()
+            ->unique('id');
+
+        if ($users->count() > 0) {
+            // fetch contacts
+            $contacts = null;
+            foreach ($users as $user) {
+                if ($user->id != Auth::user()->id) {
+                    // Get user data
+                    $userCollection = User::where('id', $user->id)->first();
+                    $contacts .= Chatify::getContactItem($request['messenger_id'], $userCollection);
+                }
+            }
+        }
+
+        // send the response
+        return Response::json([
+            'contacts' => $users->count() > 0 ? $contacts : '<br><p class="message-hint"><span>Your contatct list is empty</span></p>',
+        ], 200);
+    }
+
+    /**
+     * Update user's list item data
+     *
+     * @param Request $request
+     * @return JSON response
+     */
+    public function updateContactItem(Request $request)
+    {
+        // Get user data
+        $userCollection = User::where('id', $request['user_id'])->first();
+        $contactItem = Chatify::getContactItem($request['messenger_id'], $userCollection);
+
+        // send the response
+        return Response::json([
+            'contactItem' => $contactItem,
+        ], 200);
+    }
+
+
+    /**
+     * Search in messenger
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function search(Request $request)
+    {
+        $getRecords = null;
+        $input = trim(filter_var($request['input'], FILTER_SANITIZE_STRING));
+        $records = User::where('name', 'LIKE', "%{$input}%");
+        foreach ($records->get() as $record) {
+            $getRecords .= view('Chatify::layouts.listItem', [
+                'get' => 'search_item',
+                'type' => 'user',
+                'user' => $record,
+            ])->render();
+        }
+        // send the response
+        return Response::json([
+            'records' => $records->count() > 0
+                ? $getRecords
+                : '<p class="message-hint"><span>Nothing to show.</span></p>',
+            'addData' => 'html'
+        ], 200);
+    }
+
+    public function updateSettings(Request $request)
+    {
+        $msg = null;
+        $error = $success = 0;
+
+        // dark mode
+        if ($request['dark_mode']) {
+            $request['dark_mode'] == "dark"
+                ? User::where('id', Auth::user()->id)->update(['dark_mode' => 1])  // Make Dark
+                : User::where('id', Auth::user()->id)->update(['dark_mode' => 0]); // Make Light
+        }
+
+        // If messenger color selected
+        if ($request['messengerColor']) {
+
+            $messenger_color = explode('-', trim(filter_var($request['messengerColor'], FILTER_SANITIZE_STRING)));
+            $messenger_color = Chatify::getMessengerColors()[$messenger_color[1]];
+            User::where('id', Auth::user()->id)
+                ->update(['messenger_color' => $messenger_color]);
+        }
+        // if there is a [file]
+        if ($request->hasFile('avatar')) {
+            // allowed extensions
+            $allowed_images = Chatify::getAllowedImages();
+
+            $file = $request->file('avatar');
+            // if size less than 150MB
+            if ($file->getSize() < 150000000) {
+                if (in_array($file->getClientOriginalExtension(), $allowed_images)) {
+                    // delete the older one
+                    if (Auth::user()->avatar != config('chatify.user_avatar.default')) {
+                        $path = storage_path('app/public/' . config('chatify.user_avatar.folder') . '/' . Auth::user()->avatar);
+                        if (file_exists($path)) {
+                            @unlink($path);
+                        }
+                    }
+                    // upload
+                    $avatar = Str::uuid() . "." . $file->getClientOriginalExtension();
+                    $update = User::where('id', Auth::user()->id)->update(['avatar' => $avatar]);
+                    $file->storeAs("public/" . config('chatify.user_avatar.folder'), $avatar);
+                    $success = $update ? 1 : 0;
+                } else {
+                    $msg = "File extension not allowed!";
+                    $error = 1;
+                }
+            } else {
+                $msg = "File extension not allowed!";
+                $error = 1;
+            }
+        }
+
+        // send the response
+        return Response::json([
+            'status' => $success ? 1 : 0,
+            'error' => $error ? 1 : 0,
+            'message' => $error ? $msg : 0,
+        ], 200);
+    }
+
+    /**
+     * Set user's active status
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function setActiveStatus(Request $request)
+    {
+        $update = $request['status'] > 0
+            ? User::where('id', $request['user_id'])->update(['active_status' => 1])
+            : User::where('id', $request['user_id'])->update(['active_status' => 0]);
+        // send the response
+        return Response::json([
+            'status' => $update,
+        ], 200);
+    }
+}
