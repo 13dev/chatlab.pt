5083664859ec023ddbd32c3bee44164c413689e2
breney <brunopy2@gmail.com>
2021-01-16 19:02:22 +0000

    new migrations, avatar folder, config chatlab

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->default(config('chatlab.user_avatar.default'))->after('email');
+            $table->boolean('active_status')->default(0)->after('email');
             $table->rememberToken();
             $table->timestamps();
         });
