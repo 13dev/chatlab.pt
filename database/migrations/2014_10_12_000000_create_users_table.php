8fd054861193b71298fe2643d92a79bebf8c2c54
Leo Oliveira <13dev@users.noreply.github.com>
2021-01-16 21:18:31 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,6 @@ class CreateUsersTable extends Migration
             $table->boolean('active_status')->default(0);
             $table->timestamp('email_verified_at')->nullable();
 
-
             $table->rememberToken();
             $table->timestamps();
         });
