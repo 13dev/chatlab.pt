638085de6ee38bbba48031f7a23bd587e1879866
Leo Oliveira <13dev@users.noreply.github.com>
2021-01-21 23:57:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Validators/MessageValidator.php
+++ app/Validators/MessageValidator.php
@@ -1,17 +1,17 @@
 <?php
 
 namespace App\Validators;
+
 use Prettus\Validator\Contracts\ValidatorInterface;
 use Prettus\Validator\LaravelValidator;
 
-class MessageValidator extends LaravelValidator {
-
+class MessageValidator extends LaravelValidator
+{
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
             'thread_id' => 'required',
             'sender_id'  => 'required',
-            'body'=> 'required|max:5000'
-        ]
+            'body'=> 'required|max:5000',
+        ],
     ];
-
 }
--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace App\Validators;
+
 use Prettus\Validator\Contracts\ValidatorInterface;
 use Prettus\Validator\LaravelValidator;
 
-class UserValidator extends LaravelValidator {
-
+class UserValidator extends LaravelValidator
+{
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
             'name' => 'required|max:40',
             'email'  => 'required|email',
-            'password'=> 'required'
+            'password'=> 'required',
         ],
         ValidatorInterface::RULE_UPDATE => [
             'email' => 'required|email',
-            'password' => 'required'
-        ]
+            'password' => 'required',
+        ],
     ];
-
 }
