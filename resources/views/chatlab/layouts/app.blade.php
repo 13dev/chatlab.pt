df2d2ee522c985c448ed91ef27d863bf51a8ac21
13dev <qwerty124563@gmail.com>
2021-01-29 22:46:33 +0000

    Change logo

--- resources/views/chatlab/layouts/app.blade.php
+++ resources/views/chatlab/layouts/app.blade.php
@@ -18,11 +18,19 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-
+    <style>
+        .logo-auth {
+            width: 200px;
+            height: 200px;
+            text-align: center;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
 <body>
     @include('chatlab.modals.modals')
     @inertia
 </body>
+
 </html>
