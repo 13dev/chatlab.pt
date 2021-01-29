a81db62a13b88ac980fe184f420a3cd477955d49
13dev <qwerty124563@gmail.com>
2021-01-29 22:56:02 +0000

    Change logo

--- resources/js/components/Sidebar/Navigation.vue
+++ resources/js/components/Sidebar/Navigation.vue
@@ -6,7 +6,7 @@
                 <li class="logo">
                     <a href="">
                         <div class="loga">
-                            <img src="images/logo/logo3.png" alt="image">
+                            <img :src="'images/logo/logo' + (mode ? '3' : '5') + '.png'" alt="image">
                         </div>
 
                     </a>
